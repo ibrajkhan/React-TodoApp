f0e1a507e451faec906e92b21dbe25edf07fe008
IbrajKhan <ibraj.grd@gmail.com>
2020-07-04 10:22:14 +0530

    Adding styling App.css

---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TOdoItems() {
+  return (
+    <div className = "todo-item">
+      <input type="checkbox" />
+      <p>Some random text</p> 
+    </div>
+  );
+}
+export default TOdoItems;
