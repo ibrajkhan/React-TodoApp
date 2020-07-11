6d69b82f0c788ddb118966d4b2e1bc431492d045
IbrajKhan <ibraj.grd@gmail.com>
2020-07-11 14:10:40 +0530

    updating state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = { todos: todoData };
+    this.handelChange = this.handelChange.bind(this);
+  }
+  handelChange(id) {
+    this.setState((prevState) => {
+      const updatedTodo = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodo,
+      };
+    });
   }
   render() {
     const todoComponent = this.state.todos.map((item) => (
-      <TodoItems key={item.id} items={item} />
+      <TodoItems key={item.id} items={item} handelChange={this.handelChange} />
     ));
 
     return <div className="todo-list">{todoComponent}</div>;
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 function TOdoItems(props) {
-  console.log(props);
+  console.log(props)
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={props.items.completed} />
+      <input
+        type="checkbox"
+        checked={props.items.completed}
+        onChange={()=>props.handelChange(props.items.id)}
+      />
       <p>{props.items.text}</p>
     </div>
   );
